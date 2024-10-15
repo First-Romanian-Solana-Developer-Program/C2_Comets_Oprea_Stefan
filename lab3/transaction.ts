686ada6e551e80e8014747c7d6a4a078c045d955
opreastefan48 <opreastefan48@yahoo.com>
2024-10-15 19:25:43 +0300

    final

--- lab3/transaction.ts
+++ lab3/transaction.ts
@@ -3,26 +3,31 @@ import 'dotenv/config'
 import secret from './secret.json'
 import { Transaction, SystemProgram, sendAndConfirmTransaction, Connection, PublicKey, Keypair, LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID, getOrCreateAssociatedTokenAccount, createTransferInstruction } from '@solana/spl-token';
+import { createMemoInstruction } from "@solana/spl-memo";
 
 async function createTransaction(){
     const solanaConnection = new Connection("https://devnet.helius-rpc.com/?api-key=c3f33946-3012-4d3d-bd1e-b8f2da31a29e", "confirmed")
 
     const walletPublicKey = new PublicKey("Stef5nMVQmKg1XGKD4bsb5tXue2QKFaxkC2MXybdnre");
     const devWallet = Keypair.fromSecretKey(new Uint8Array(secret)); 
-    console.log(devWallet.publicKey.toBase58())
     const transaction = new Transaction().add(
         SystemProgram.transfer({
           fromPubkey: devWallet.publicKey,
           toPubkey: walletPublicKey,
-          lamports: 0.002 * LAMPORTS_PER_SOL,
+          lamports: 0.02 * LAMPORTS_PER_SOL,
         })
       );
-      const solFeeSignature =await sendAndConfirmTransaction(solanaConnection, transaction, [devWallet]);
+    const sendMemo = createMemoInstruction( "aaaaaaaaaaaaaaaaaaaaaaaaaaaa!");
+    transaction.add(sendMemo);
     
-      if (!solFeeSignature) {
-        console.error("SOL fee transfer failed");
-      }
-      console.log(solFeeSignature);
+    const solFeeSignature =await sendAndConfirmTransaction(solanaConnection, transaction, [devWallet]);
+
+    if (!solFeeSignature) {
+    console.error("Failed sending sol.");
+    }
+    console.log(solFeeSignature);
+
+
 }
 
 createTransaction()
